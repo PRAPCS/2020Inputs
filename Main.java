d3859ef6a5bc6f2f5fe1d8e48ed9d37bcb7e6736
Mr. Burger <bburger@d155.org>
2020-09-02 18:01:30 +0000

    Added Scanner

--- Main.java
+++ Main.java
@@ -1,5 +1,30 @@
+
+import java.util.Scanner; //imports the scanner class to use methods
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+
+    Scanner input = new Scanner(System.in);// creates input scanner object to be usen in the program
+
+    int num1;
+    double num2;
+    double product;
+
+    System.out.println("Please input a number");
+    num1 = input.nextInt();
+
+    System.out.println("You entered: " + num1);
+
+    System.out.println("Please input another number");
+    num2 = input.nextDouble();
+    System.out.println("You entered: " + num2);
+
+    product = num1 * num2;
+
+    System.out.println("The product of the two numbers is: " + product);
+
+    System.out.println(1/2); //both values are int so answer is an int
+    System.out.println(1/2.0); //if one is a double then the answer will be a double
+
   }
 }
